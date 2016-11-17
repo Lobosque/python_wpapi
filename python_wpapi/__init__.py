bd6437ce5f9175d4292ba4c4705e22759935362b
Lucas Lobosque <lucaslobosque@gmail.com>
2016-11-17 15:04:24 -0200

    version bump

--- python_wpapi/__init__.py
+++ python_wpapi/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Lucas Lobosque"""
 __email__ = 'lucas.lobosque@contentools.com'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='python_wpapi',
-    version='0.2.1',
+    version='0.3.0',
     description="Simple wrapper around the Wordpress REST API",
     long_description=readme + '\n\n' + history,
     author="Lucas Lobosque",
